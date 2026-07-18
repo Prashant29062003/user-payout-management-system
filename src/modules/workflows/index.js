038ba5dc10a86312026b35bea26a677257ef5c36
Prashant-Kumar <prashantkumar6425@gmail.com>
2026-07-19 03:25:27 +0530

    feat: export WithdrawalWorkflow from withdrawal.workflow.js to index

--- src/modules/workflows/index.js
+++ src/modules/workflows/index.js
@@ -1,2 +1,3 @@
 export { AdvancePayoutWorkflow, advancePayoutWorkflow } from './advance-payout.workflow.js';
 export { SaleReconciliationWorkflow, saleReconciliationWorkflow } from './sale-reconciliation.workflow.js';
+export { WithdrawalWorkflow, withdrawalWorkflow } from './withdrawal.workflow.js';
